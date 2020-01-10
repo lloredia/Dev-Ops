979b120015f1c46b2a330c789dede36c48cb6636
An=onymous881453 <53010259+lloredia@users.noreply.github.com>
2020-01-10 10:50:13 -0500

    Create install.sh

---
+++ splunk/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O splunk-8.0.1-6db836e2fb9e-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.0.1&product=splunk&filename=splunk-8.0.1-6db836e2fb9e-Linux-x86_64.tgz&wget=true'
