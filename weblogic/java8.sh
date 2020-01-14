1afb22f3fb5adcd0a18ea07093568c13f0108d99
An=onymous881453 <53010259+lloredia@users.noreply.github.com>
2020-01-14 10:00:40 -0500

    Create java8.sh

---
+++ weblogic/java8.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+##Download JavaDK-8u221
+
+wget -O jdk-8u221-linux-x64.tar.gz \
+  -c --content-disposition \
+  "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=239835_230deb18db3e4014bb8e3e8324f81b43"
