54a48160bc938bc262d577b2a2c32b8f7aabc403
An=onymous881453 <53010259+lloredia@users.noreply.github.com>
2020-01-14 15:35:07 -0500

    Update rhel_install.sh

--- Docker/rhel_install.sh
+++ Docker/rhel_install.sh
@@ -1,3 +1,25 @@
 #!/bin/bash 
+#Installation on rhel(os)
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf repolist -v
+ sleep 2
+  clear
+sudo dnf install -y docker-ce-3:18.09.1-3.el7
+ sleep 2
+  clear
+sudo dnf install --nobest docker-ce -y
+ sleep 2
+  clear
+sudo dnf install -y  https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+                clear
+                        echo "Installation is complete..."
+                sleep 3
+                        clear
+
+sudo systemctl enable docker
+
+sudo systemctl start docker
+
+sudo systemctl status docker
 
 
