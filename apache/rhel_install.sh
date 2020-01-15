2b8c7073122d576347dbf5c2c88ce489d5a766d5
An=onymous881453 <53010259+lloredia@users.noreply.github.com>
2020-01-15 13:33:59 -0500

    Update rhel_install.sh

--- apache/rhel_install.sh
+++ apache/rhel_install.sh
@@ -18,3 +18,18 @@ sleep 5
         clear
  sudo systemctl status httpd
 
+FILE=/var/www/html/index.html
+if [ -f "$FILE" ]; then
+    echo "$FILE exist"
+ else
+        echo "$FILE does not exist"
+sleep 2
+               sudo touch /var/www/html/index.html
+        echo "$FILE was just created sucessfully..."
+fi
+sleep 3
+        clear
+sudo unlink index.html
+ln -s /var/www/html/index.html ./
+~
+
